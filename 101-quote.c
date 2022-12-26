2969edad208048e7e5b5ab1050b5ab3779a6100b
Johnson Masino <johnsonmasino@gmail.com>
2022-12-26 12:15:11 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints a line of code using printf
+ * return: 1
+ * code by Masino
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
