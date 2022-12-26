1fe562515cc0867f2fd87a57108dd05381339f2f
Johnson Masino <johnsonmasino@gmail.com>
2022-12-26 01:03:34 -0800

    4. Hello, puts

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a line of code using puts function by Masino
+ * Code by Masino
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
