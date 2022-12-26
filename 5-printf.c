e16a8ffd81bf6c08956dc3cb58d02f9f32f887a9
Johnson Masino <johnsonmasino@gmail.com>
2022-12-26 01:15:32 -0800

    5. Hello, printf

---
+++ 5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a line of code and a new line using printf
+ * Return: 0
+ * Code by Masino
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a peice of art,\n");
+	return (0);
+}
