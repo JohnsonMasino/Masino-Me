4ab156bef621e67ccff8643d7dad6bba39affeab
Johnson Masino <johnsonmasino@gmail.com>
2022-12-28 11:14:24 -0800

    3. alphABET

--- 3-print_alphabets.c
+++ 3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /*
- * main - prints alphabets in lowercase using printf
- * return: 0
+ * main - prints alphabet in lowercase and then in upper case
+ * Return: 0
  * Code by Masino
  */
 
 int main(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 	putchar('\n');
-
 	return (0);
 }
