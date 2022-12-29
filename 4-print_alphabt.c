981dbc72be6bb773a3da081011a9148417aedc87
Johnson Masino <johnsonmasino@gmail.com>
2022-12-29 02:22:21 -0800

    4.4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - prints alphabets in lowercase using putchar
+ * Return: Always 0 (Success)
+ * Code by Masino
+ */
+
+int main(void)
+{
+	char letter;
+
+	putchar(letter == 'a'; <= 'z' letter++)
+		putchar("\n");
+	return (0);
+}
