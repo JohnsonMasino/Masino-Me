dd1725997265acbe70310ff9b4d356593cb003a5
Johnson Masino <johnsonmasino@gmail.com>
2022-12-30 23:55:52 -0800

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+ * main - Prints combination of different numbers,
+ * Return: Always 0 (Success)
+ * Code by Masino
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 +1; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
