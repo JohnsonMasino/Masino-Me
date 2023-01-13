f5a00a195bf47e71762a3315d310f5e90324588e
Johnson Masino <johnsonmasino@gmail.com>
2023-01-13 03:36:17 -0800

    Masino's world

---
+++ masino.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints hello world and a new line using printf
+ * Return: Always 0 (Success)
+ * Code by  Masino
+ */
+
+int main(void)
+{
+	printf("Masino's World\n");
+	return (0);
+}
