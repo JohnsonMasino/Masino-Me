3eb5c39cd006d14ca4d6750bd47b0b42c7ad9432
JohnsonMasino <johnsonmasino@gmail.com>
2023-01-24 05:07:20 -0800

    prints numbers 1-10

---
+++ numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - prints numbers from 1 - 10.
+ * Return: 0.
+ * Code by Masino
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a >= 1; a <= 10; a++)
+		printf("%d\n", a);
+	return (0);
+}
